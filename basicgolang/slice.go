01cca6d5244a4fe425791f4b8684be51ae990e12
sonypiay <pyscho30@gmail.com>
2026-06-19 01:48:42 +0700

    slice

--- basicgolang/slice.go
+++ basicgolang/slice.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// names := [...]string{"Sony", "Budi", "Supri", "Eko"}
+	// names := [...]string{"Sony", "Budi", "Supri", "Eko", "Kurniawan", "Khannedy"}
 
-	// slice1 := names[1:3]
+	// slice1 := names[4:6]
 	// fmt.Println(slice1)
 
 	// slice2 := names[:3]
@@ -17,14 +17,11 @@ func main() {
 	// var slice4 []string = names[:]
 	// fmt.Println(slice4)
 
-	days := [...]string{"senin","selasa","rabu","kamis","jumat","sabtu","minggu"}
+	days := [...]string{"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"}
 
 	daySlice1 := days[5:]
-	fmt.Println(daySlice1)
-
 	daySlice1[0] = "Sabtu Baru"
 	daySlice1[1] = "Minggu Baru"
-	fmt.Println(daySlice1)
 	fmt.Println(days)
 
 	daySlice2 := append(daySlice1, "Libur Baru")
@@ -32,4 +29,28 @@ func main() {
 	fmt.Println(daySlice1)
 	fmt.Println(daySlice2)
 	fmt.Println(days)
+
+	var newSlice []string = make([]string, 2, 5)
+	newSlice[0] = "Eko"
+	newSlice[1] = "Eko"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Eko")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Budi"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
 }
